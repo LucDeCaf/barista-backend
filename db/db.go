f0123f7dc5ab3731c85882b8b96dd909d076b52c
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2024-09-03 15:49:57 +0200

    bruh

--- db/db.go
+++ db/db.go
@@ -3,13 +3,15 @@ package db
 import (
 	"database/sql"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
 func init() {
 	var err error
-	DB, err = sql.Open("sqlite3", "../db.db")
+	DB, err = sql.Open("sqlite3", "db.db")
 	if err != nil {
 		log.Fatal(err)
 	}
